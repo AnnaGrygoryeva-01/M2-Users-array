0b4354004d7b69ae2477952d7383504021dda3d2
Anna Grygoryeva <annagrygoryeva3101@gmail.com>
2026-01-16 21:27:50 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Функція повертає масив користувачів старших за заданий вік. У порядку зростання.  З перевіркою наявності потрібних ключів.
+
+const users = [
+  { name: "John", age: 25, isMale: true },
+  { name: "Jane", age: 30, isMale: false },
+  { name: "Mike", age: 20, isMale: true },
+  { name: "Emily", age: 27, isMale: false },
+  { name: "Tom", age: 35, isMale: true },
+];
